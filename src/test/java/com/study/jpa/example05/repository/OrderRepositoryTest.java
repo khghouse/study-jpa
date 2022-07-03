45849a663accc698fe8bf1fe22aef02d9bc10045
khghouse <khghouse@gmail.com>
2022-07-04 00:15:01 +0900

    5장 연관관계 매핑 기초 실전 예제

--- src/test/java/com/study/jpa/example05/repository/OrderRepositoryTest.java
+++ src/test/java/com/study/jpa/example05/repository/OrderRepositoryTest.java
@@ -1,5 +1,43 @@
 package com.study.jpa.example05.repository;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.study.jpa.example05.entity.Member;
+import com.study.jpa.example05.entity.Order;
+import com.study.jpa.example05.enums.OrderStatus;
+
+@SpringBootTest
 public class OrderRepositoryTest {
 
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Test
+	public void createTest() throws Exception {
+
+		Member member = Member.builder()
+				.name("홍길동")
+				.city("서울시")
+				.street("올림픽대로")
+				.zipcode("12345")
+				.build();
+		System.out.println("member : " + member);
+		memberRepository.save(member);
+
+		Order order = Order.builder()
+				.member(member)
+				.orderDate(LocalDateTime.now())
+				.status(OrderStatus.ORDER)
+				.build();
+
+		System.out.println("order : " + order);
+		orderRepository.save(order);
+	}
 }
