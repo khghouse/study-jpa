adc132468c281d160f5ba91e86bc0936b9fb43e1
khghouse <khghouse@naver.com>
2022-07-20 00:25:28 +0900

    mockito MemberRepository create, delete test

--- chapter05/src/test/java/com/study/jpa/repository/MemberRepositoryMockitoTest.java
+++ chapter05/src/test/java/com/study/jpa/repository/MemberRepositoryMockitoTest.java
@@ -1,12 +1,15 @@
 package com.study.jpa.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -20,22 +23,55 @@ public class MemberRepositoryMockitoTest {
 	@Mock
 	private MemberRepository memberRepository;
 
-	@Test
-	void readTest() {
-		// given
-		Member member = Member.builder()
+	private Member member;
+
+	@BeforeEach
+	void setUp() {
+		member = Member.builder()
 				.name("홍길동")
 				.city("서울시")
 				.street("올림픽대로")
 				.zipcode("12345")
 				.build();
+	}
+
+	@Test
+	void readTest() {
+		// given
+		when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
 
 		// when
+		Member selectedMember = memberRepository.findById(anyLong()).get();
+
+		// then
+		verify(memberRepository, times(1)).findById(anyLong());
+		assertThat(selectedMember).isEqualTo(member);
+	}
+
+	@Test
+	void createTest() {
+		// given
+		when(memberRepository.save(any())).thenReturn(member);
+
+		// when
+		Member savedMember = memberRepository.save(any());
+
+		// then
+		verify(memberRepository).save(any());
+		assertThat(savedMember).isEqualTo(member);
+	}
+
+	@Test
+	void deleteTest() {
+		// given
 		when(memberRepository.findById(anyLong())).thenReturn(Optional.of(member));
-		System.out.println(memberRepository.findById(anyLong()));
+
+		// when
+		Member selectedMember = memberRepository.findById(anyLong()).get();
+		memberRepository.delete(selectedMember);
 
 		// then
 		verify(memberRepository).findById(anyLong());
-		assertEquals(Optional.of(member), memberRepository.findById(anyLong()));
+		verify(memberRepository).delete(selectedMember);
 	}
 }
